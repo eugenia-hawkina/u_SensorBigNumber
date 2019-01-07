89c6ebf1576676aabd08b35c6c5016a9bc3d6546
User <User@Acer.mynet>
2019-01-07 09:21:15 +0200

    first commit

---
+++ src/main/java/telran/ashkelon2018/SensorBigNumberApplication.java
@@ -0,0 +1,14 @@
+package telran.ashkelon2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorBigNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorBigNumberApplication.class, args);
+	}
+
+}
+
